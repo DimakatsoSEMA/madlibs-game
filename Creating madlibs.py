1bdfde97ca1c7875f3247d9e291772d85f601a9f
Dimakatso Sema <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-04-07 14:21:04 +0200

    Add madlibs word game.

---
+++ Creating madlibs.py
@@ -0,0 +1,30 @@
+#Creating madlibs  
+#a word game where one player prompts others for words to fill in blanks in a story, 
+#resulting in often funny and nonsensical narratives
+#
+
+# #Tell me about yourself'
+
+# a = input('sex: ')
+# b = input('location: ')
+# c = input('favorite color: ')
+# d = input('hobby: ')
+
+# my_story = f'I am a {a}, I live in {b}, my favorite colour is {c}. I enjoy {d}. It is nice to meet you.'
+# print(my_story)
+
+#Tell me a story COVID-19
+
+Year = input('year: ')
+virus = input('virus name: ')
+res1 = input('restriction: ')
+res2 = input('restriction: ')
+res3 = input('restriction: ')
+feeling1 = input('feeling: ')
+feeling2 = input('feeling: ')
+
+tell_me_a_story = f'So, in {Year}, the people in South africa were sitting around when one couple arrived from their trip and brought the nasty {virus} with them.\
+We all wanted them gone then all of a sudden we were in lockdowns with different levels and restrictions.Some examples of the restrictions were {res1}, {res2},and {res3}.\
+People had to get vaccinated. But years later. We are all {feeling1} and {feeling2}.'
+
+print(tell_me_a_story)
